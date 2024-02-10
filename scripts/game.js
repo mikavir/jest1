6208362bc992dd12192dbe3ccc6b8d9479cc84c7
Mika Virtucio <mikavirtucio@gmail.com>
2024-02-10 15:51:13 +0000

    Add index and game object

---
+++ scripts/game.js
@@ -0,0 +1,15 @@
+// Javascript structures
+// game { object }
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+}
+module.exports = { game };
+// newGame ()
+// addTurns ()
+// showTurns ()
+// lightsOn()
+// playerTurns()
+// showSquare ()
