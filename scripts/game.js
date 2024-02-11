be80bdc64289d205b9bcfbd0e5a08288b5af5bcb
Mika Virtucio <mikavirtucio@gmail.com>
2024-02-11 20:29:45 +0000

    Refactor data-listener attribute

--- scripts/game.js
+++ scripts/game.js
@@ -57,6 +57,18 @@ let game = {
 function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
+    for (let circle of document.getElementsByClassName("circle")){
+        if (circle.getAttribute("data-listener") !== "true") {
+            circle.addEventListener("click", (e) => {
+                let move = e.target.getAttribute("id");
+                lightsOn(move);
+                game.playerMoves.push(move);
+                playerTurn();
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+
+    }
     game.score = 0;
     showScore();
     addTurn();
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -57,6 +57,12 @@ describe("newGame works correctly", () => {
     test("should display 0 for the element id of score", () =>{
         expect(document.getElementById("score").innerText).toEqual(0);
     });
+    test("expect data-listener to be true", () => {
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            expect(element.getAttribute("data-listener")).toBe("true");
+        }
+    });
 
 });
 
