fd6d46a11ca0c04e8597b9c661000360fa3aaff9
Mika Virtucio <mikavirtucio@gmail.com>
2024-02-11 19:28:44 +0000

    Add lightsOn function and fix addTurn bug

--- scripts/game.js
+++ scripts/game.js
@@ -1,33 +1,81 @@
-// Javascript structures
-// game { object }
+// // Javascript structures
+// // game { object }
+// let game = {
+//     score: 0,
+//     currentGame: [],
+//     playerMoves: [],
+//     choices: ["button1", "button2", "button3", "button4"],
+// }
+
+// // newGame ()
+// function newGame () {
+//     game.score = 0;
+//     game.playerMoves = [];
+//     game.currentGame = [];
+//     showScore();
+//     addTurn();
+// }
+
+// // addTurns () 1) clear playerMoves, 2)randomly add a buttonID to current game array 3) call shoowTurns
+// function addTurn() {
+//     game.playerMoves = [];
+//     game.currentGame.push(game.choices[(Math.floor(Math.random)* 4)]);
+//     //showTurns()
+// }
+// // showTurns ()
+// // lightsOn()
+// function lightsOn2(circ) {
+//     document.getElementById(circ).classList.add("light");
+//     setTimeout(() => {
+//         document.getElementById(circ).classList.remove("light");
+//     }, 400);
+// } 
+
+// function lightsOn(circ) {
+//     document.getElementById(circ).classList.add("light");
+//     setTimeout(function () {
+//         document.getElementById(circ).classList.remove("light");
+//     }, 400);
+// }
+
+// // playerTurns()
+// // showScore ()
+// function showScore () {
+//     document.getElementById("score").innerText = game.score;
+// }
+
+// module.exports = { game, newGame, showScore, addTurn, lightsOn };
+
 let game = {
-    score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: ["button1", "button2", "button3", "button4"],
-}
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+};
 
-// newGame ()
-function newGame () {
-    game.score = 0;
-    game.playerMoves = [];
+function newGame() {
     game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
     showScore();
     addTurn();
 }
 
-// addTurns () 1) clear playerMoves, 2)randomly add a buttonID to current game array 3) call shoowTurns
 function addTurn() {
     game.playerMoves = [];
-    game.currentGame.push(game.choices[(Math.floor(Math.random)* 4)]);
-    //showTurns()
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
 }
-// showTurns ()
-// lightsOn()
-// playerTurns()
-// showScore ()
-function showScore () {
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+
+function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore, addTurn };
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
